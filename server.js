e0588a5ca3cf77dc323af60c705e45f5fe7ecc98
Josh Trommel <joshtrommel@gmail.com>
2015-04-05 02:55:40 -0700

    this code broken af lmao Jacob's about to help a pal out

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // call packages hard ballin af
-var https      = require('https');
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
-var Client     = require('node-wolfram');
-var Wolfram    = new Client(process.env.API_KEY);
+var https      = require('https'),
+    http       = require('http'),
+    url        = require('url'),
+    express    = require('express'),
+    app        = express(),
+    bodyParser = require('body-parser'),
+    parseString = require('xml2js').parseString;
+
 
 // Use bodyParser() and get data from POST
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -16,66 +18,27 @@ var port = process.env.PORT || 8080
 // API Routes
 var router = express.Router();
 
-// The main shit here boys
-router.get('/', function(request, response) {
-  Wolfram.query(request.query.i, function(error, result) {
-      if (error) {
-        console.error(error);
-      } else {
-        // API:
-        // First result (0) contains info on user input
-        // Second result contains result/input of input
-        // Third result contains misc extra answers such as word value of a #
-        // Fourth result contains extra misc shit we don't need to worry abt
-        // Fifth result contains even more misc shit. Get to this later
+function getPage(response, page) {
+  http.request(url.parse(page), function(res) {
+    res.setEncoding('utf8');
 
-        if (result.queryresult.$.success) {
-          // Status returned is gucci
-          try {
+    var buffer = [];
 
-            var final = {
-              success: true,
-              type:   result.queryresult.$.datatypes,
-              input:  result.queryresult.pod[0].subpod[0].plaintext[0],
-              result: {
-                plaintext: result.queryresult.pod[1].subpod[0].plaintext[0],
-              }
-            }
+    res.on('data', function(chunk) {
+      buffer.push(chunk);
+    });
 
-            switch(final.type) {
-              case "City,Weather":
-                break
-              case "Math":
-                final["result"]["words"] = result.queryresult.pod[2].subpod[0].plaintext[0]
-                final["result"]["plaintext"] = final["result"]["plaintext"]
-                break
-              case "Food":
-                break
-              case "Word":
-                var plaintext = final.result.plaintext // 1 | noun | the fleshy part of the human body that you sit on
-                var dict = plaintext.split(" | ") // [1, "noun", ]
+    res.on('end', function() {
+      response.send(buffer);
+    });
 
-                delete final["result"]["plaintext"]
-                dict.splice(0, 1);
-
-                final["result"]["word type"]  = dict[0]
-                final["result"]["definition"] = dict[1].replace('\n2', ''); // "the fleshy part of the human body that you sit on"
-                break
-              default:
-            }
-
-            // Shoot of the Jason (best/fav son) response
-            response.json({result: final});
-          } catch(err) {
-            // The fuck did the user search for
-            // Adam's probably fucking around again smh
-            response.json({result: {success: false, input: request.query.i}})
-          }
-        }
-
-      }
   });
+}
 
+// The main shit here boys
+var url = "http://api.wolframalpha.com/v2/query?appid=" + process.env.API_KEY + "&input=" + request.query.i + "&format=plaintext"
+router.get('/', function(request, response) {
+  var xml = getPage(response, url)
 })
 
 // All routes prefix with /input
@@ -83,3 +46,61 @@ app.use('/input', router);
 
 // Start the server man!!
 app.listen(port);
+
+
+
+// if (error) {
+//   console.error(error);
+// } else {
+//   // API:
+//   // First result (0) contains info on user input
+//   // Second result contains result/input of input
+//   // Third result contains misc extra answers such as word value of a #
+//   // Fourth result contains extra misc shit we don't need to worry abt
+//   // Fifth result contains even more misc shit. Get to this later
+//
+//   if (result.queryresult.$.success) {
+//     // Status returned is gucci
+//     try {
+//
+//       var final = {
+//         success: true,
+//         type:   result.queryresult.$.datatypes,
+//         input:  result.queryresult.pod[0].subpod[0].plaintext[0],
+//         result: {
+//           plaintext: result.queryresult.pod[1].subpod[0].plaintext[0],
+//         }
+//       }
+//
+//       switch(final.type) {
+//         case "City,Weather":
+//           break
+//         case "Math":
+//           final["result"]["words"] = result.queryresult.pod[2].subpod[0].plaintext[0]
+//           final["result"]["plaintext"] = final["result"]["plaintext"]
+//           break
+//         case "Food":
+//           break
+//         case "Word":
+//           var plaintext = final.result.plaintext // 1 | noun | the fleshy part of the human body that you sit on
+//           var dict = plaintext.split(" | ") // [1, "noun", ]
+//
+//           delete final["result"]["plaintext"]
+//           dict.splice(0, 1);
+//
+//           final["result"]["word type"]  = dict[0]
+//           final["result"]["definition"] = dict[1].replace('\n2', ''); // "the fleshy part of the human body that you sit on"
+//           break
+//         default:
+//       }
+//
+//       // Shoot of the Jason (best/fav son) response
+//       response.json({result: final});
+//     } catch(err) {
+//       // The fuck did the user search for
+//       // Adam's probably fucking around again smh
+//       response.json({result: {success: false, input: request.query.i}})
+//     }
+//   }
+//
+// }
