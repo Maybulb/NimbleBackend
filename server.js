eeaca9e53d3d38efd3c4a09b958f7925daa74877
Josh Trommel <joshtrommel@gmail.com>
2015-04-05 14:57:45 -0700

    Removed W|A module. Everything's relatively self-sufficient now?

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // call packages hard ballin af
-var https      = require('https'),
     http       = require('http'),
     url        = require('url'),
     express    = require('express'),
@@ -39,8 +38,59 @@ function getPage(url, callback) {
 // The main shit here boys
 router.get('/', function(request, response) {
   var apiUrl = "http://api.wolframalpha.com/v2/query?appid=" + process.env.API_KEY + "&input=" + request.query.i + "&format=plaintext";
-  getPage(apiUrl, function(json) {
-    response.json({result: json})
+  getPage(apiUrl, function(result) {
+
+    // API:
+    // First result (0) contains info on user input
+    // Second result contains result/input of input
+    // Third result contains misc extra answers such as word value of a #
+    // Fourth result contains extra misc shit we don't need to worry abt
+    // Fifth result contains even more misc shit. Get to this later
+
+    if (result.queryresult.$.success) {
+      // Status returned is gucci
+      try {
+
+        var json = {
+          success: true,
+          type:   result.queryresult.$.datatypes,
+          input:  result.queryresult.pod[0].subpod[0].plaintext[0],
+          result: {
+            plaintext: result.queryresult.pod[1].subpod[0].plaintext[0],
+          }
+        }
+
+        switch(json.type) {
+          case "City,Weather":
+            break
+          case "Math":
+            json["result"]["words"] = result.queryresult.pod[2].subpod[0].plaintext[0]
+            json["result"]["plaintext"] = json["result"]["plaintext"]
+            break
+          case "Food":
+            break
+          case "Word":
+            var plaintext = json.result.plaintext // 1 | noun | the fleshy part of the human body that you sit on
+            var dict = plaintext.split(" | ") // [1, "noun", ]
+
+            delete json["result"]["plaintext"]
+            dict.splice(0, 1);
+
+            json["result"]["word type"]  = dict[0]
+            json["result"]["definition"] = dict[1].replace('\n2', ''); // "the fleshy part of the human body that you sit on"
+            break
+          default:
+        }
+
+        // Shoot of the Jason (best/fav son) response
+        response.json({result: json});
+      } catch(err) {
+        // The fuck did the user search for
+        // Adam's probably fucking around again smh
+        response.json({result: {success: false, input: request.query.i}})
+      }
+    }
+
   });
 });
 
@@ -49,56 +99,3 @@ app.use('/input', router);
 
 // Start the server man!!
 app.listen(port);
-
-
-
-// // API:
-// // First result (0) contains info on user input
-// // Second result contains result/input of input
-// // Third result contains misc extra answers such as word value of a #
-// // Fourth result contains extra misc shit we don't need to worry abt
-// // Fifth result contains even more misc shit. Get to this later
-//
-// if (result.queryresult.$.success) {
-//   // Status returned is gucci
-//   try {
-//
-//     var final = {
-//       success: true,
-//       type:   result.queryresult.$.datatypes,
-//       input:  result.queryresult.pod[0].subpod[0].plaintext[0],
-//       result: {
-//         plaintext: result.queryresult.pod[1].subpod[0].plaintext[0],
-//       }
-//     }
-//
-//     switch(final.type) {
-//       case "City,Weather":
-//         break
-//       case "Math":
-//         final["result"]["words"] = result.queryresult.pod[2].subpod[0].plaintext[0]
-//         final["result"]["plaintext"] = final["result"]["plaintext"]
-//         break
-//       case "Food":
-//         break
-//       case "Word":
-//         var plaintext = final.result.plaintext // 1 | noun | the fleshy part of the human body that you sit on
-//         var dict = plaintext.split(" | ") // [1, "noun", ]
-//
-//         delete final["result"]["plaintext"]
-//         dict.splice(0, 1);
-//
-//         final["result"]["word type"]  = dict[0]
-//         final["result"]["definition"] = dict[1].replace('\n2', ''); // "the fleshy part of the human body that you sit on"
-//         break
-//       default:
-//     }
-//
-//     // Shoot of the Jason (best/fav son) response
-//     response.json({result: final});
-//   } catch(err) {
-//     // The fuck did the user search for
-//     // Adam's probably fucking around again smh
-//     response.json({result: {success: false, input: request.query.i}})
-//   }
-// }
