a95edaa9a2a4a81bbe0649032e477f12c5da559e
Josh Trommel <joshtrommel@gmail.com>
2015-04-05 03:09:18 -0700

    killl me slowly

--- server.js
+++ server.js
@@ -20,26 +20,25 @@ var router = express.Router();
 
 function getPage(response, page) {
   http.request(url.parse(page), function(res) {
-    res.setEncoding('utf8');
-
-    var buffer = [];
+    var buffer = '';
 
     res.on('data', function(chunk) {
-      buffer.push(chunk);
+      buffer += chunk;
     });
 
     res.on('end', function() {
       response.send(buffer);
     });
-
   });
 }
 
+
 // The main shit here boys
-var url = "http://api.wolframalpha.com/v2/query?appid=" + process.env.API_KEY + "&input=" + request.query.i + "&format=plaintext"
 router.get('/', function(request, response) {
-  var xml = getPage(response, url)
-})
+  var apiUrl = "http://api.wolframalpha.com/v2/query?appid=" + process.env.API_KEY + "&input=" + request.query.i + "&format=plaintext";
+  var xml = getPage(response, apiUrl);
+  response.send(xml)
+});
 
 // All routes prefix with /input
 app.use('/input', router);
@@ -49,58 +48,53 @@ app.listen(port);
 
 
 
-// if (error) {
-//   console.error(error);
-// } else {
-//   // API:
-//   // First result (0) contains info on user input
-//   // Second result contains result/input of input
-//   // Third result contains misc extra answers such as word value of a #
-//   // Fourth result contains extra misc shit we don't need to worry abt
-//   // Fifth result contains even more misc shit. Get to this later
+// // API:
+// // First result (0) contains info on user input
+// // Second result contains result/input of input
+// // Third result contains misc extra answers such as word value of a #
+// // Fourth result contains extra misc shit we don't need to worry abt
+// // Fifth result contains even more misc shit. Get to this later
 //
-//   if (result.queryresult.$.success) {
-//     // Status returned is gucci
-//     try {
+// if (result.queryresult.$.success) {
+//   // Status returned is gucci
+//   try {
 //
-//       var final = {
-//         success: true,
-//         type:   result.queryresult.$.datatypes,
-//         input:  result.queryresult.pod[0].subpod[0].plaintext[0],
-//         result: {
-//           plaintext: result.queryresult.pod[1].subpod[0].plaintext[0],
-//         }
+//     var final = {
+//       success: true,
+//       type:   result.queryresult.$.datatypes,
+//       input:  result.queryresult.pod[0].subpod[0].plaintext[0],
+//       result: {
+//         plaintext: result.queryresult.pod[1].subpod[0].plaintext[0],
 //       }
-//
-//       switch(final.type) {
-//         case "City,Weather":
-//           break
-//         case "Math":
-//           final["result"]["words"] = result.queryresult.pod[2].subpod[0].plaintext[0]
-//           final["result"]["plaintext"] = final["result"]["plaintext"]
-//           break
-//         case "Food":
-//           break
-//         case "Word":
-//           var plaintext = final.result.plaintext // 1 | noun | the fleshy part of the human body that you sit on
-//           var dict = plaintext.split(" | ") // [1, "noun", ]
-//
-//           delete final["result"]["plaintext"]
-//           dict.splice(0, 1);
-//
-//           final["result"]["word type"]  = dict[0]
-//           final["result"]["definition"] = dict[1].replace('\n2', ''); // "the fleshy part of the human body that you sit on"
-//           break
-//         default:
-//       }
-//
-//       // Shoot of the Jason (best/fav son) response
-//       response.json({result: final});
-//     } catch(err) {
-//       // The fuck did the user search for
-//       // Adam's probably fucking around again smh
-//       response.json({result: {success: false, input: request.query.i}})
 //     }
-//   }
 //
+//     switch(final.type) {
+//       case "City,Weather":
+//         break
+//       case "Math":
+//         final["result"]["words"] = result.queryresult.pod[2].subpod[0].plaintext[0]
+//         final["result"]["plaintext"] = final["result"]["plaintext"]
+//         break
+//       case "Food":
+//         break
+//       case "Word":
+//         var plaintext = final.result.plaintext // 1 | noun | the fleshy part of the human body that you sit on
+//         var dict = plaintext.split(" | ") // [1, "noun", ]
+//
+//         delete final["result"]["plaintext"]
+//         dict.splice(0, 1);
+//
+//         final["result"]["word type"]  = dict[0]
+//         final["result"]["definition"] = dict[1].replace('\n2', ''); // "the fleshy part of the human body that you sit on"
+//         break
+//       default:
+//     }
+//
+//     // Shoot of the Jason (best/fav son) response
+//     response.json({result: final});
+//   } catch(err) {
+//     // The fuck did the user search for
+//     // Adam's probably fucking around again smh
+//     response.json({result: {success: false, input: request.query.i}})
+//   }
 // }
