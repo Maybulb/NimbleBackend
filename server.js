ba88dcd1c3ed5604d939165c7161a17f1fd766b9
Ethan Arterberry <ethanarterberry@gmail.com>
2016-01-19 17:43:58 -0800

    I hope this will work dammit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var wolfram = require('wolfram-alpha').createClient(process.env.API_KEY);
+var wolfram = require('wolfram-alpha').createClient(process.env.API_KEY, {
+  width: 360
+});
 var port, router;
 
 
